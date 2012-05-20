b1ebf29a20407f5e08a623b042a49d999fe1a76c
Alex Kalderimis <alex@flymine.org>
2012-05-20 18:54:36 +0100

    Support for quotes and comments

--- js/parsing/test.js
+++ js/parsing/test.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 // Read file contents
 var data = fs.readFileSync('js/parsing/scheem.grammar', 'utf-8');
 // Create my parser
-var parse = PEG.buildParser(data).parse;
+var parse = PEG.buildParser(data, {trackLineAndColumn: true}).parse;
 
 // Do a test
 assert.equal(
@@ -19,6 +19,13 @@ assert.deepEqual(
     ['a', 'b', 'c'],
     'Flat list'
 );
+
+assert.deepEqual( 
+    parse('(a b c 1 2 3)'), 
+    ['a', 'b', 'c', 1, 2, 3],
+    'Flat list with numbers'
+);
+
 
 assert.deepEqual(
     parse('(+ x (f a b))'),
@@ -45,7 +52,41 @@ assert.deepEqual(
 );
 
 assert.deepEqual(
-    parse("(+ x  \n\t\t(  f a b))"),
+    parse("(+ x  \n\t\t(  f a b)\n)\t"),
     ['+', 'x', ['f', 'a', 'b']],
     'Multiple whitespaces 3 - special spaces'
 );
+
+var fib = fs.readFileSync('js/parsing/fib.scheem', 'utf-8');
+
+assert.deepEqual(
+    parse(fib),
+    ['define', ['fib', 'n'], 
+        ['define', ['ff', 'm', 'c', 'n-1', 'n-2'],
+          ['define', 'next', ['+', 'n-1', 'n-2']],
+          ['if', ['=', 'm', 'c'], 'next', ['ff', 'm', ['+', 1, 'c'], 'next', 'n-1']]],
+        ['if', ['<', 'n', 2], 'n', ['ff', 'n', 2, 1, 0]]],
+
+    'Linear recursive fib' 
+);
+
+assert.deepEqual(
+    parse("'x"),
+    ["quote", "x"],
+    "Quoting atom"
+);
+
+assert.deepEqual(
+    parse("'(a b c)"),
+    ["quote", ["a", "b", "c"]],
+    "Quote list"
+);
+
+assert.deepEqual(
+    parse("(a 'b '(c d))"),
+    ["a", ["quote", "b"], ["quote", ["c", "d"]]],
+    "Quote combo"
+);
+
+
+
